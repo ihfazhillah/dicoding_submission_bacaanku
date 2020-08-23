8234da53fb8acee560407b2d63c12e1d2012620c
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-08-23 20:59:34 +0700

    Add about page

--- app/src/main/java/com/ihfazh/bacaanku/AboutActivity.java
+++ app/src/main/java/com/ihfazh/bacaanku/AboutActivity.java
@@ -2,13 +2,34 @@ package com.ihfazh.bacaanku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private TextView tvEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setTitle("About");
+        }
+
+        tvEmail = findViewById(R.id.tv_email);
+        tvEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.putExtra(Intent.EXTRA_EMAIL, tvEmail.getText().toString());
+                if (intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
