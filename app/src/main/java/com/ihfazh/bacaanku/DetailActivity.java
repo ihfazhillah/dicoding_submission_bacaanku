8d9629bc9efcb8b33def275a138b18c0309f6cb1
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-08-23 19:29:20 +0700

    add search click

--- app/src/main/java/com/ihfazh/bacaanku/DetailActivity.java
+++ app/src/main/java/com/ihfazh/bacaanku/DetailActivity.java
@@ -2,12 +2,29 @@ package com.ihfazh.bacaanku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.ihfazh.bacaanku.data.Book;
+import com.ihfazh.bacaanku.data.JsonReader;
+
+import org.json.JSONException;
+
+import java.io.IOException;
 
 public class DetailActivity extends AppCompatActivity {
 
     final public static String BOOK_ID = "book_id";
+    private TextView tvTitle, tvAuthor, tvDescription;
+    private ImageView img;
+    private Button btnSearch;
+    private Book book;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +33,41 @@ public class DetailActivity extends AppCompatActivity {
 
         String bookId = getIntent().getStringExtra(BOOK_ID);
 
-        Toast.makeText(this, "Hello from " + bookId, Toast.LENGTH_SHORT).show();
+        tvTitle = findViewById(R.id.tv_title);
+        tvAuthor = findViewById(R.id.tv_author);
+        tvDescription = findViewById(R.id.tv_description);
+
+        img = findViewById(R.id.img_book);
+
+        btnSearch = findViewById(R.id.cari_penjual);
+
+        try {
+            JsonReader reader = new JsonReader(this);
+            book = reader.getBookById(bookId);
+            displayDetailData();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void displayDetailData() {
+        Glide.with(this)
+                .load(book.getImageUrl())
+                .into(img);
+        tvTitle.setText(book.getTitle());
+        tvAuthor.setText(book.getAuthor());
+        tvDescription.setText(book.getDescription());
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                intent.putExtra(SearchManager.QUERY, "Beli buku " + book.getTitle());
+                startActivity(intent);
+            }
+        });
+
     }
 }
