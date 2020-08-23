55271cd745b76cd65090f8ea41c192176c2a610a
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-08-23 13:23:12 +0700

    init

---
+++ app/src/main/java/com/ihfazh/bacaanku/data/Attribute.java
@@ -0,0 +1,21 @@
+package com.ihfazh.bacaanku.data;
+
+public class Attribute {
+    private String key, value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
---
+++ app/src/main/java/com/ihfazh/bacaanku/data/Book.java
@@ -0,0 +1,57 @@
+package com.ihfazh.bacaanku.data;
+
+import java.util.List;
+
+public class Book {
+    private int id;
+    private String title, description, author, imageUrl;
+    private List<Attribute> attributeList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Attribute> getAttributeList() {
+        return attributeList;
+    }
+
+    public void setAttributeList(List<Attribute> attributeList) {
+        this.attributeList = attributeList;
+    }
+}
